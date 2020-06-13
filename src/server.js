b83d85f3bc9b2a272f4734575220d7c460c3fe2a
Juana Paiva <juanapaiva2019@gmail.com>
2020-06-13 20:18:11 -0300

    Utilizando Template Engine, criando partials e layout; alterações de style; +Nunjucks

--- src/server.js
+++ src/server.js
@@ -4,14 +4,26 @@ const server = express();
 // Configurando pasta public
 server.use(express.static("public"));
 
+// Utilizando Template Engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+});
+
 // Rota Home
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  return res.render("index.html");
 });
 
 // Rota Create Point
 server.get("/create-point", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html");
+  return res.render("create-point.html");
+});
+
+// Rota Search Results
+server.get("/search", (req, res) => {
+  return res.render("search-results.html");
 });
 
 // Ligar o servidor na porta 3000
