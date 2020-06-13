f21bfaabc89815327de5aeb07b212698ea808ee2
Juana Paiva <juanapaiva2019@gmail.com>
2020-06-12 21:07:15 -0300

    Ítens, modal Pesquisar pontos de coleta e página Search Results + styles e scripts referentes; atributo required no form de cadastro; separação do style do Header; e algumas alterações

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,9 +1,14 @@
-function getStateOrCityOption(select, url) {
+//#region States and Cities configuration
+
+function getStateOrCityOption(select, url, city) {
+  if (city) select.innerHTML = "<option value>Selecione a Cidade</option>";
+  
   fetch(url)
     .then((res) => res.json())
     .then((values) => {
       for(const value of values) {
-        select.innerHTML += `<option value="${value.id}">${value.nome}</option>`;
+        const auxValue = (city) ? value.nome : value.id;
+        select.innerHTML += `<option value="${auxValue}">${value.nome}</option>`;
       }
     });
 }
@@ -12,7 +17,7 @@ function populateUFs() {
   const ufSelect = document.querySelector("select[name=uf]");
   const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
-  getStateOrCityOption(ufSelect, url);
+  getStateOrCityOption(ufSelect, url, false);
 }
 
 function populateCities(event) {
@@ -27,13 +32,48 @@ function populateCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-  getStateOrCityOption(citySelect, url);
+  getStateOrCityOption(citySelect, url, true);
   citySelect.disabled = false;
 }
 
 populateUFs();
 
 document
-  .querySelector("select[name=uf]")
-  .addEventListener("change", populateCities);
-  
+.querySelector("select[name=uf]")
+.addEventListener("change", populateCities);
+
+//#endregion
+
+//#region Items configuration
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  // Adiciona e remove uma classe com JavaScript
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex((item) => item == itemId);
+
+  if (alreadySelected != -1) {
+    const filteredItems = selectedItems.filter((item) => item != itemId);
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId)
+  }
+
+  collectedItems.value = selectedItems;
+}
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+//#endregion
