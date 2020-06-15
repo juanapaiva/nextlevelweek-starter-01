1963558ad4403e8fe2a6e2c70ef1dbd3d21c9cd7
Juana Paiva <juanapaiva2019@gmail.com>
2020-06-14 22:11:38 -0300

    Salvando dados do cadastro; modal Point Created e style

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db");
 // Configurando pasta public
 server.use(express.static("public"));
 
+// Habilitando o udo do req.body
+server.use(express.urlencoded({ extended: true }));
+
 // Utilizando Template Engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
@@ -24,6 +27,45 @@ server.get("/create-point", (req, res) => {
   return res.render("create-point.html");
 });
 
+server.post("/savepoint", (req, res) => {
+  // Inserir dados
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err);
+      return res.send('Erro no cadastro');
+    }
+
+    console.log('Cadastrado com sucesso!');
+    console.log(this);
+
+    return res.render("create-point.html", { saved: true });
+  }
+
+  db.run(query, values, afterInsertData);
+});
+
 // Rota Search Results
 server.get("/search", (req, res) => {
   // Pegar dados
@@ -32,7 +74,7 @@ server.get("/search", (req, res) => {
       return console.log(err);
 
     const total = rows.length;
-    
+
     // Renderizar página
     return res.render("search-results.html", { places: rows, total });
   });
